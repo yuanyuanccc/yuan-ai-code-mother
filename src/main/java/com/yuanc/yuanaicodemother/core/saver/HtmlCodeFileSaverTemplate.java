02eb4cc7ea8ebcc06a1a1512202771b7f0fe2026
Yuanc <718297950@qq,com>
2026-03-08 20:00:12 +0800

    4.AI生成应用

---
+++ src/main/java/com/yuanc/yuanaicodemother/core/saver/HtmlCodeFileSaverTemplate.java
@@ -0,0 +1,35 @@
+package com.yuanc.yuanaicodemother.core.saver;
+
+import cn.hutool.core.util.StrUtil;
+import com.yuanc.yuanaicodemother.ai.model.HtmlCodeResult;
+import com.yuanc.yuanaicodemother.ai.model.enums.CodeGenTypeEnum;
+import com.yuanc.yuanaicodemother.exception.BusinessException;
+import com.yuanc.yuanaicodemother.exception.ErrorCode;
+
+/**
+ * HTML代码文件保存器
+ *
+ * @author yupi
+ */
+public class HtmlCodeFileSaverTemplate extends CodeFileSaverTemplate<HtmlCodeResult> {
+
+    @Override
+    protected CodeGenTypeEnum getCodeType() {
+        return CodeGenTypeEnum.HTML;
+    }
+
+    @Override
+    protected void saveFiles(HtmlCodeResult result, String baseDirPath) {
+        // 保存 HTML 文件
+        writeToFile(baseDirPath, "index.html", result.getHtmlCode());
+    }
+
+    @Override
+    protected void validateInput(HtmlCodeResult result) {
+        super.validateInput(result);
+        // HTML 代码不能为空
+        if (StrUtil.isBlank(result.getHtmlCode())) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "HTML代码内容不能为空");
+        }
+    }
+}
---
+++ src/test/java/com/yuanc/yuanaicodemother/ai/AiCodeGeneratorServiceTest.java
@@ -0,0 +1,27 @@
+package com.yuanc.yuanaicodemother.ai;
+
+import com.yuanc.yuanaicodemother.ai.model.HtmlCodeResult;
+import com.yuanc.yuanaicodemother.ai.model.MultiFileCodeResult;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeGeneratorServiceTest {
+
+    @Resource
+    private AiCodeGeneratorService aiCodeGeneratorService;
+
+    @Test
+    void generateHtmlCode() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("你好");
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void generateMultiFileCode() {
+        MultiFileCodeResult multiFileCode = aiCodeGeneratorService.generateMultiFileCode("做个程序员鱼皮的留言板");
+        Assertions.assertNotNull(multiFileCode);
+    }
+}
