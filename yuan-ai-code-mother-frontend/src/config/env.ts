a7bdcdb2ba34658522fe5c62bb9d8a87e1904342
Yuanc <718297950@qq,com>
2026-03-15 22:30:39 +0800

    Revert "7.VUE工程项目生成(前端)"
    This reverts commit cb1e47186459e2fefbf553551a3b72033476c5c4.

--- yuan-ai-code-mother-frontend/src/config/env.ts
+++ yuan-ai-code-mother-frontend/src/config/env.ts
@@ -1,7 +1,6 @@
 /**
  * 环境变量配置
  */
-import {CodeGenTypeEnum} from "@/utils/codeGenTypes.ts";
 
 // 应用部署域名
 export const DEPLOY_DOMAIN = import.meta.env.VITE_DEPLOY_DOMAIN || 'http://localhost'
@@ -19,10 +18,5 @@ export const getDeployUrl = (deployKey: string) => {
 
 // 获取静态资源预览URL
 export const getStaticPreviewUrl = (codeGenType: string, appId: string) => {
-  const baseUrl = `${STATIC_BASE_URL}/${codeGenType}_${appId}/`
-  // 如果是 Vue 项目，浏览地址需要添加 dist 后缀
-  if (codeGenType === CodeGenTypeEnum.VUE_PROJECT) {
-    return `${baseUrl}dist/index.html`
-  }
-  return baseUrl
+  return `${STATIC_BASE_URL}/${codeGenType}_${appId}/`
 }
